60644d51e4b8c056ff35ae15baf093053449fc9d
Kiminni <imk0980@gmail.com>
2024-03-23 04:01:58 +0900

    get method 적용

---
+++ api/src/main/java/org/springboot/api/controller/GetController.java
@@ -0,0 +1,56 @@
+package org.springboot.api.controller;
+
+import org.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping(value="name")
+    public String getName(){
+        return "Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization) {
+        return "name: " + name + ", email: " + email + ", organization: " + organization;
+    }
+
+    //어떤 값이 들어올지 모르는 경우
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n" );
+        });
+        return sb.toString();
+    }
+
+    @GetMapping(value="/request3")
+    public String getRequestParam3(MemberDto memberDto) {
+        // return memberDto.getName() + ", " + memberDto.getEmail() + ", " + memberDto.getOrganization();
+        return memberDto.toString();
+    }
+}
---
+++ api/src/main/java/org/springboot/api/dto/MemberDto.java
@@ -0,0 +1,24 @@
+package org.springboot.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+    private String name;
+    private String email;
+    private String organization;
+
+    @Override
+    public String toString() {
+        return "MemberDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", organization='" + organization + '\'' +
+                '}';
+    }
+
+
+
+}
