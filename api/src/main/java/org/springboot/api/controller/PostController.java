588b63f121fb845d2ef46181a5f3f1d1250febf5
Kiminni <imk0980@gmail.com>
2024-03-23 05:30:20 +0900

    swagger 설정

--- api/src/main/java/org/springboot/api/controller/PostController.java
+++ api/src/main/java/org/springboot/api/controller/PostController.java
@@ -1,14 +1,20 @@
 package org.springboot.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springboot.api.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
 
+    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
+    @Parameter(name = "str", description = "2번 반복할 문자열")
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
