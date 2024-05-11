3394ce8ed58274a0419ae642712d7a74e2527f0c
Kiminni <imk0980@gmail.com>
2024-05-12 01:51:29 +0900

    다대다 양방향 매핑 테스트

--- relationship/src/main/java/org/springboot/relationship/data/entity/Product.java
+++ relationship/src/main/java/org/springboot/relationship/data/entity/Product.java
@@ -3,6 +3,8 @@ package org.springboot.relationship.data.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="product")
@@ -36,4 +38,12 @@ public class Product extends BaseEntity {
     @JoinColumn(name = "provider_id")
     @ToString.Exclude
     private Provider provider;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Producer> producers = new ArrayList<>();
+
+    public void addProducer(Producer producer){
+        producers.add(producer);
+    }
 }
--- relationship/src/test/java/org/springboot/relationship/data/repository/ProducerRepositoryTest.java
+++ relationship/src/test/java/org/springboot/relationship/data/repository/ProducerRepositoryTest.java
@@ -44,12 +44,39 @@ class ProducerRepositoryTest {
 
     }
 
+    @Test
+    @Transactional
+    void relationshipTest2() {
+        Product product1 = saveProduct("스프링 부트 JPA", 5000, 500);
+        Product product2 = saveProduct("펜", 200, 100);
+        Product product3 = saveProduct("가방", 10000, 1000);
+
+        Producer producer1 = saveProducer("쿠팡");
+        Producer producer2 = saveProducer("11번가");
+
+        producer1.addProduct(product1);
+        producer1.addProduct(product2);
+        producer2.addProduct(product2);
+        producer2.addProduct(product3);
+
+        product1.addProducer(producer1);
+        product2.addProducer(producer1);
+        product2.addProducer(producer2);
+        product3.addProducer(producer2);
+
+        producerRepository.saveAll(List.of(producer1, producer2));
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        System.out.println("products: " + producerRepository.findById(producer1.getId()).get());
+
+        System.out.println("producers: " + productRepository.findById(product1.getNumber()).get());
+    }
+
     private Product saveProduct(String name, int price, int stock) {
-        Product product = Product.builder()
-                .name(name)
-                .price(price)
-                .stock(stock)
-                .build();
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
         return productRepository.save(product);
     }
 
